1d6daf3e4ad3b03ddffac5933266c4953fea96b7
Florian HEMBERT <flhember@e2r7p5.42.fr>
2019-11-11 14:54:54 +0100

    push nul

--- srcs/creat_adja_lst.c
+++ srcs/creat_adja_lst.c
@@ -6,16 +6,15 @@
 /*   By: flhember <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 16:25:07 by flhember          #+#    #+#             */
-/*   Updated: 2019/11/08 17:24:55 by flhember         ###   ########.fr       */
+/*   Updated: 2019/11/11 14:05:36 by flhember         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-t_lst		creat_adja_lst(t_stock *room, t_data *env)
+t_lst		*creat_adja_lst(t_stock *room, t_data *env)
 {
 	t_lst	lst;
-
 
 	if (!(lst->tab = ft_memalloc(sizeof(t_room * env->nb_room))))
 		return (NULL);
