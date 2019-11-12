5a20900c50e382bd03048d39dc4ff488bb45af88
Charles COUTURE <chcoutur@e3r8p7.42.fr>
2019-11-12 15:37:43 +0100

    Modification check du split, reste a checker les tubes

--- srcs/parsing_map.c
+++ srcs/parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: flhember <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 19:26:44 by flhember          #+#    #+#             */
-/*   Updated: 2019/11/11 17:41:35 by flhember         ###   ########.fr       */
+/*   Updated: 2019/11/12 15:36:14 by chcoutur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,43 +22,59 @@ int check_nb_ants(char *str, t_data *env)
 	int i;
 
 	i = 0;
-	if (add_flag(env, ANTS) == 0 && (env->se & ASTART) == 0 && (env->se & AEND) == 0)
+	if (add_flag(env, ANTS) == 0
+			&& (env->se & ASTART) == 0 && (env->se & AEND) == 0)
 	{
 		env->flags |= ANTS;
-	while (str[i])
-	{
-		if (ft_isdigit(str[i]) == 1)
-			i++;
+		while (str[i])
+		{
+			if (ft_isdigit(str[i]) == 1)
+				i++;
+			else
+				return (-1);
+		}
+		if (ft_atoi(str) > 0)
+			return (1);
 		else
-			return (0);
-	}
-	return (1);
+			return (-1);
 	}
 	return (0);
 }
 
+int nb_split(char **tab)
+{
+	int i;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	return (i);
+}
+
+int check_maillon(char **tab)
+{
+	if (nb_split(tab) != 3
+			|| (ft_strisdigit(tab[1]) != 1 || ft_strisdigit(tab[2]) != 1))
+		return (-1);
+	return (1);
+}
+
 int check_valid_room(char *str, t_data *env, t_stock **lst)
 {
-	char **split;
+	char **tab;
 
-	(void)env;
-	split = NULL;
-	if (ft_count_c(str, ' ') != 2)
-		return (0);
-	else
+	tab = NULL;
+	if (!(tab = ft_strsplit(str, ' ')))
+		return (-1);
+	if (tab[0][0] == 'L' || tab[0][0] == '#' || check_maillon(tab) != 1)
 	{
-		split = ft_strsplit(str, ' ');
-		if (split[0][0] == 'L' || split[0][0] == '#')
-		{
-			ft_free_tab_char(split);
-			return (0);
-		}
-		creat_maillon(lst, split[0], split[1], split[2], 0);
-		env->nb_room++;
-		ft_free_tab_char(split);
-		free(split);
+		ft_free_tab_char(tab);
+		return (-1);
 	}
-	//printf("-> [%s]\n", str);
+	creat_maillon(lst, tab[0], tab[1], tab[2], 0);
+	env->nb_room++;
+	ft_free_tab_char(tab);
+	free(tab);
 	if ((env->se & ASTART) != 0)
 	{
 		env->se |= PSTART;
@@ -74,13 +90,15 @@ int check_valid_room(char *str, t_data *env, t_stock **lst)
 
 int check_start_end(char *str, t_data *env, t_stock **lst)
 {
-	if (ft_strcmp(str + 2, "start") == 0 && add_flag(env, START) == 0 && (env->se & AEND) == 0)
+	if (ft_strcmp(str + 2, "start") == 0
+			&& add_flag(env, START) == 0 && (env->se & AEND) == 0)
 	{
 		env->flags |= START;
 		env->se |= ASTART;
 		return (1);
 	}
-	else if (ft_strcmp(str + 2, "end") == 0 && add_flag(env, END) == 0 && (env->se & ASTART) == 0)
+	else if (ft_strcmp(str + 2, "end") == 0
+			&& add_flag(env, END) == 0 && (env->se & ASTART) == 0)
 	{
 		env->flags |= END;
 		env->se |= AEND;
@@ -91,7 +109,7 @@ int check_start_end(char *str, t_data *env, t_stock **lst)
 		check_valid_room(str, env, lst);
 		return (1);
 	}
-	return (0);
+	return (-1);
 }
 
 int check_room(char *str, t_data *env, t_stock **lst)
@@ -103,17 +121,21 @@ int check_room(char *str, t_data *env, t_stock **lst)
 	}
 	else if (str[0] == '#')
 		return (1);
-	return (0);
+	return (-1);
 }
 
 int check_tube(char *str, t_data *env)
 {
-	if ((env->se & PSTART) == 0 || (env->se & PEND) == 0)
-		return (0);
+	(void)env;
+	//stockage connexion
+	//check si plusieurs '-'
+
+	//if ((env->se & PSTART) == 0 || (env->se & PEND) == 0)
+	//	return (0);
 	if (ft_count_c(str, '-') == 1)
 		return (1);
 	else
-		return (0);
+		return (-1);
 }
 
 int check_line(char *str, t_data *env, t_stock **lst)
@@ -129,7 +151,8 @@ int check_line(char *str, t_data *env, t_stock **lst)
 		if (check_nb_ants(str, env) == 1)
 			return (1);
 	}
-	else if (ft_is_c(str, '-'))
+	else if (ft_is_c(str, '-')
+			&& (env->se & PSTART) != 0 && (env->se & PEND) != 0)
 	{
 		if (check_tube(str, env) == 1)
 			return (1);
@@ -139,7 +162,7 @@ int check_line(char *str, t_data *env, t_stock **lst)
 		if (check_valid_room(str, env, lst) == 1)
 			return (1);
 	}
-	return (0);
+	return (-1);
 }
 
 int		parsing_map(t_data *env, t_stock **lst)
@@ -148,16 +171,24 @@ int		parsing_map(t_data *env, t_stock **lst)
 
 	while ((line = ft_get_fd(0)))
 	{
-		//printf("[%s]\n", line);
-		if (check_line(line, env, lst) == 0)
+		if (check_line(line, env, lst) != 1)
 		{
 			free(line);
 			printf("MAP KO\n");
-			return (0);
+			return (-1);
 		}
 		free(line);
 	}
-	add_flag(env, START) && add_flag(env, END) ? printf("MAP OK\n") : printf("MAP KO\n");
 	print_lst(lst);
-	return (0);
+	printf("\n");
+	if (add_flag(env, START) && add_flag(env, END))
+	{
+		printf("MAP OK\n");
+		return (1);
+	}
+	else
+	{
+		printf("MAP KO\n");
+		return (-1);
+	}
 }
