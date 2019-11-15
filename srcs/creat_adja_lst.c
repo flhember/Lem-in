1b742652ab5d86f8ed0df1420b2ab10659ca2c57
Florian HEMBERT <flhember@e1r4p11.42.fr>
2019-11-15 20:22:25 +0100

    list adja finit

--- srcs/creat_adja_lst.c
+++ srcs/creat_adja_lst.c
@@ -6,7 +6,7 @@
 /*   By: flhember <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 16:25:07 by flhember          #+#    #+#             */
-/*   Updated: 2019/11/15 14:48:25 by flhember         ###   ########.fr       */
+/*   Updated: 2019/11/15 19:22:29 by flhember         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,6 @@ void		fill_tab_room(t_stock **room_ptr, t_lst **lst, size_t i)
 		return ;
 	(*lst)->tab[k]->pos = k;
 	(*lst)->tab[k]->name = ft_strdup(room->room);
-//	ft_printf("test %s -> [%lu] s ->[%d] e-> [%d]\n", (*lst)->tab[k]->name, i, room->start, room->end);
 	(*lst)->tab[k]->x = room->x;
 	(*lst)->tab[k]->y = room->y;
 	(*lst)->tab[k]->start = room->start;
