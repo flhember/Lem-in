456b2a1366150dc26b3928e11b3fbee4ffd29a2c
Charles COUTURE <chcoutur@e1r8p15.42.fr>
2019-11-21 15:45:32 +0100

    Update -> fixing leaks

--- srcs/algo_main.c
+++ srcs/algo_main.c
@@ -6,13 +6,13 @@
 /*   By: flhember <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 16:42:48 by flhember          #+#    #+#             */
-/*   Updated: 2019/11/20 16:52:12 by flhember         ###   ########.fr       */
+/*   Updated: 2019/11/21 15:40:03 by chcoutur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lem_in.h>
 
-void		find_start(t_lst **lst, t_data *env)
+/*void		find_start(t_lst **lst, t_data *env)
 {
 	int		i;
 	
@@ -23,11 +23,12 @@ void		find_start(t_lst **lst, t_data *env)
 		if ((*lst)->tab[i]->start == 1)
 			env->start = i;
 	}
-}
+}*/
 
 int			algo_main(t_lst **lst, t_data *env)
 {
-	find_start(lst, env);
+	(void)lst;
+//	find_start(lst, env);
 	printf("start est au maillon = %d\n", env->start);
 	return (0);
 }
