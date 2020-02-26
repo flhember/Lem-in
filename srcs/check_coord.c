47be5c0d13af10d2513ad757abcd1690533a2d35
Charles <chachac77@gmail.com>
2020-02-26 22:58:53 +0100

    Correction limit int corrige

--- srcs/check_coord.c
+++ srcs/check_coord.c
@@ -18,12 +18,15 @@ int		check_coord(char *str)
 	int		size;
 
 	size = ft_strlen(str);
-	if (size > 10)
+	if (size > 11)
 		return (-1);
-	if (size == 10 || size == 11)
+	else if (size == 10)
 	{
 		if ((ret = ft_strcmp(str, "2147483647")) > 0)
 			return (-1);
+	}
+	else if (size == 11)
+	{
 		if ((ret = ft_strcmp(str, "-2147483648")) > 0)
 			return (-1);
 	}
